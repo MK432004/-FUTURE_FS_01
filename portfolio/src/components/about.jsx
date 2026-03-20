5a18623679ee2bdcb692faad854460ab50c5f674
Manishkumar <manish089808@gmail.com>
2026-03-20 18:44:21 +0530

    apload fille

---
+++ portfolio/src/components/about.jsx
@@ -0,0 +1,19 @@
+function About(){
+  return(
+
+    <section className="about">
+
+      <h1>About Me</h1>
+
+      <p>
+      Hi, I'm Manish Kumar. I am a passionate web developer.
+      I build modern web applications using JavaScript and React.
+      I enjoy solving problems and learning new technologies.
+      </p>
+
+    </section>
+
+  )
+}
+
+export default About
